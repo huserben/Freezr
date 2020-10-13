4929c96f32f54cac2ded625c1c4f2c3704afe8a6
Benj Huser <benj.huser@gmail.com>
2020-10-13 10:32:33 +0200

    Add dummy backend

---
+++ Freezr.Backend/Freezr.Backend/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Freezr.Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly ILogger<TestController> logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Works";
+        }
+    }
+}
